d20b08756f949d6cdc1abe274a6c5d2944888903
Your <scarlet3628@likelion.org>
2018-10-18 17:04:26 +0900

    sidebar

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,25 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Category of Post
-# Category.create(name: "Tech")
-# Category.create(name: "Geeky & Ridiculous")
-# Category.create(name: "Home & Kitchen")
-# Category.create(name: "Travel")
-# Category.create(name: "Outdoor & Sports")
-# Category.create(name: "Pet Sutff")
+Category.create(name: "Tech")
+Category.create(name: "Geeky & Ridiculous")
+Category.create(name: "Home & Kitchen")
+Category.create(name: "Travel")
+Category.create(name: "Outdoor & Sports")
+Category.create(name: "Pet Sutff")
 
 
 # Category of Dealzon
-# Acategory.create(name: "Sales & Offers")
-# Acategory.create(name: "Deals")
-# Acategory.create(name: "Promotions")
-# Acategory.create(name: "Promo Codes")
+Acategory.create(name: "Sales & Offers")
+Acategory.create(name: "Deals")
+Acategory.create(name: "Promotions")
+Acategory.create(name: "Promo Codes")
 
 # Category of Event
-# Ecategory.create(name: "Featured Sales")
-# Ecategory.create(name: "Deals")
-# Ecategory.create(name: "Promo Codes")
-# Ecategory.create(name: "Interesing")
+Ecategory.create(name: "Featured Sales")
+Ecategory.create(name: "Deals")
+Ecategory.create(name: "Promo Codes")
+Ecategory.create(name: "Interesing")
 
 # Category of Blog
 # Bcategory.create(name: "blog_01")
