3e21761f682fbd925af0b0699396c19895d8f3d8
Your <scarlet3628@likelion.org>
2018-10-18 15:07:05 +0900

    deals

--- app/controllers/dealzons_controller.rb
+++ app/controllers/dealzons_controller.rb
@@ -17,13 +17,13 @@ class DealzonsController < ApplicationController
      @acategory_id = Acategory.find_by(name: params[:acategory]).id
      @dealzons = Dealzon.where(acategory_id: @acategory_id).order("created_at DESC").page(params[:page]).per(40)
     end
-     @dealzon_side = Dealzon.order("impressions_count DESC").limit(10)
+     @dealzon_side = Dealzon.order("impressions_count DESC").limit(5)
      @blogs = Blog.all.order("created_at DESC").limit(1)
   end
 
   def show 
     @dealzon_show = Dealzon.all.order("created_at DESC").limit(8)
-    @dealzon_side = Dealzon.order("impressions_count DESC").limit(10)
+    @dealzon_side = Dealzon.order("impressions_count DESC").limit(5)
     @blogs = Blog.all.order("created_at DESC").limit(1)
   end
 
