6710dbefc79c8db13fa434da8e264fcca0d50922
Your <scarlet3628@likelion.org>
2018-10-27 15:49:48 +0900

    sns

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,5 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus weibo qq douban google_bookmark
-                          delicious tumblr pinterest linkedin wechat vkontakte
-                          reddit telegram)
+  config.allow_sites = %w(twitter facebook google_plus tumblr pinterest linkedin)
 end
